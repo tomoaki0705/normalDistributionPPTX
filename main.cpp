e9d2d8ab4e614553045bd95edb573bcaf1927df5
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2019-11-14 11:36:22 +0900

    middle of refactor

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 #include <vector>
 
 const int cm2pptx = 360000;
-const int offsetX = 7 * cm2pptx;
-const int offsetY = 5 * cm2pptx;
+const int cStepCounts = 100;
+const double cCanvasOffsetX = 7;
+const double cCanvasOffsetY = 5;
+const double cCanvasWidth = 12;
+const double cCanvasHeight = 9;
 const double cMinRange = -3.0;
 const double cMaxRange = 3.0;
-const int cCanvasWidth = (int)(cMaxRange - cMinRange) * cm2pptx;
-const double cVerticalScale = 8.;
 
 double function(double x)
 {
@@ -22,6 +23,9 @@ double function(double x)
 
 struct _point
 {
+    struct _point(double _x, double _y)
+        : x(_x), y(_y) {};
+    struct _point() : x(0.), y(0.) {};
     double x;
     double y;
 };
@@ -29,10 +33,21 @@ typedef _point point;
 
 struct _size
 {
+    struct _size(double _width, double _height)
+        : width(_width), height(_height) {};
     double width;
     double height;
 };
 typedef _size size;
+
+struct _range
+{
+    struct _range(double _min, double _max)
+        : min(_min), max(_max) {};
+    double min;
+    double max;
+};
+typedef _range range;
 
 struct bezierPoint
 {
@@ -45,20 +60,120 @@ enum objectType
     OBJECT_STRAIGHTLINE
 };
 
+double convertToCanvasY(double y, double maxRangeY, double minRangeY, double canvasHeight)
+{
+    double ratioY = canvasHeight / (maxRangeY - minRangeY);
+    double result = (y - minRangeY) * ratioY;
+    result = canvasHeight - result;
+    return result;
+}
+
+point convertToCanvas(const point& figurePoint, const range& _rangeX, const range& _rangeY, const size& canvasSize)
+{
+    point result;
+    double ratioX = canvasSize.width / (_rangeX.max - _rangeX.min);
+    result.x = (figurePoint.x - _rangeX.min) * ratioX;
+    result.y = convertToCanvasY(figurePoint.y, _rangeY.max, _rangeY.min, canvasSize.height);
+    return result;
+}
+
+point convertToCanvas(const point& figurePoint, const double minRangeX, const double maxRangeX, const double minRangeY, const double maxRangeY, const int canvasWidth, const int canvasHeight)
+{
+    return convertToCanvas(figurePoint, range(minRangeX, maxRangeX), range(minRangeY, maxRangeY), size(canvasWidth, canvasHeight));
+}
+
 class baseObject
 {
 public:
-    baseObject(enum objectType _type, const size& _canvasSize, const size& _offset)
+    baseObject(enum objectType _type)
         : type(_type)
-        , canvasSize(_canvasSize)
-        , canvasOffset(_offset)
+        , rangeY(range(DBL_MAX, -DBL_MAX))
+        , rangeX(range(DBL_MAX, -DBL_MAX))
     {};
     ~baseObject();
+    void drawCurves(double(*func)(double), const range& _rangeX, const int cDivSteps)
+    {
+        rangeX = _rangeX;
+        double cStep = (rangeX.max - rangeX.min) / cDivSteps;
+
+        // for differentials
+        bezierPoint dPoint;
+        double dStep = cStep / 3.;
+
+        for (size_t i = 0; i <= cDivSteps; i++)
+        {
+            double x = rangeX.min + i * cStep;
+            point pt(x, func(x));
+            points.push_back(pt);
+
+            // check the range of Y
+            rangeY.min = pt.y < rangeY.min ? pt.y : rangeY.min;
+            rangeY.max = pt.y > rangeY.max ? pt.y : rangeY.max;
+
+            // compute differentials
+            double slope = (func(pt.x + dStep) - func(pt.x - dStep)) / 2;
+            dPoint.pt[1] = point(pt.x - dStep, pt.y - slope);
+            dPoint.pt[2] = pt;
+            if (i != 0)
+            { // skip first iteration
+                curves.push_back(dPoint);
+            }
+            dPoint.pt[0] = point(pt.x + dStep, pt.y + slope);
+        }
+    }
+    enum objectType getType() const { return type; }
+    range getRangeX() const { return rangeX; }
+    range getRangeY() const { return rangeY; }
+    void drawObject(std::ostream& os, unsigned int counter, const size& _canvasOffset, const size& _canvasSize) const
+    {
+        int canvasOffsetPPTXWidth = (int)(_canvasOffset.width * cm2pptx);
+        int canvasOffsetPPTXHeight = (int)(_canvasOffset.height * cm2pptx);
+        int canvasSizePPTXWidth = (int)(_canvasSize.width * cm2pptx);
+        int canvasSizePPTXHeight = (int)(_canvasSize.height * cm2pptx);
+        os << "<p:sp><p:nvSpPr><p:cNvPr id=\"" << counter << "\" name=\"qqqqqqqqq\"><a:extLst><a:ext uri=\"{FF2B5EF4-FFF2-40B4-BE49-F238E27FC236}\"><a16:creationId xmlns:a16=\"http://schemas.microsoft.com/office/drawing/2014/main\" id=\"{29FFCF3D-8F32-482F-B193-ACC1CF50B0FA}\"/></a:ext></a:extLst></p:cNvPr><p:cNvSpPr/><p:nvPr/></p:nvSpPr>";
+        os << "<p:spPr><a:xfrm><a:off x=\"" << canvasOffsetPPTXWidth << "\" y=\"" << canvasOffsetPPTXHeight << "\"/>" << std::endl;
+        os << "<a:ext cx=\"" << canvasOffsetPPTXWidth << "\" cy=\"" << canvasSizePPTXHeight << "\"/>" << std::endl << "</a:xfrm><a:custGeom><a:avLst/><a:gdLst>" << std::endl;
+        for (size_t i = 0; i < points.size(); i++)
+        {
+            point canvasPoint = convertToCanvas(points[i], rangeX, rangeY, size(canvasSizePPTXWidth, canvasSizePPTXHeight));
+            os << "<a:gd name=\"connsiteX" << i << "\"  fmla=\"*/ " << (int)(canvasPoint.x) << " w " << canvasSizePPTXWidth  << "\"/>" << std::endl;
+            os << "<a:gd name=\"connsiteY" << i << "\"  fmla=\"*/ " << (int)(canvasPoint.y) << " h " << canvasSizePPTXHeight << "\"/>" << std::endl;
+        }
+        os << "</a:gdLst><a:ahLst/><a:cxnLst>" << std::endl;
+        for (size_t i = 0; i < points.size(); i++)
+        {
+            os << "<a:cxn ang=\"0\">" << std::endl;
+            os << "<a:pos x=\"connsiteX" << i << "\" y=\"connsiteY" << i << "\"/>" << std::endl;
+            os << "</a:cxn>" << std::endl;
+        }
+        {
+            point canvasPointStart = convertToCanvas(points[0], rangeX, rangeY, size(canvasSizePPTXWidth, canvasSizePPTXHeight));
+            os << "</a:cxnLst><a:rect l=\"l\" t=\"t\" r=\"r\" b=\"b\"/><a:pathLst>" << std::endl;
+            os << "<a:path w=\"" << canvasSizePPTXWidth << "\" h=\"" << canvasSizePPTXHeight << "\">" << std::endl;
+            os << "<a:moveTo>" << std::endl;
+            os << "<a:pt x=\"" << (int)(canvasPointStart.x) << "\" y=\"" << (int)(canvasPointStart.y) << "\"/>" << std::endl;
+            os << "</a:moveTo>" << std::endl;
+        }
+        for (auto&& it : curves)
+        {
+            os << "<a:cubicBezTo>" << std::endl;
+            for (size_t i = 0; i < 3; i++)
+            {
+                point canvasPoint = convertToCanvas(it.pt[i], rangeX, rangeY, size(canvasSizePPTXWidth, canvasSizePPTXHeight));
+                os << "<a:pt x=\"" << (int)(canvasPoint.x) << "\" y=\"" << (int)(canvasPoint.y) << "\"/>" << std::endl;
+            }
+            os << "</a:cubicBezTo>" << std::endl;
+        }
+        os << "</a:path>" << std::endl << "</a:pathLst>" << std::endl << "</a:custGeom><a:noFill/></p:spPr>" << std::endl;
+        os << "<p:style><a:lnRef idx=\"2\"><a:schemeClr val=\"accent1\"><a:shade val=\"50000\"/></a:schemeClr></a:lnRef><a:fillRef idx=\"1\"><a:schemeClr val=\"accent1\"/></a:fillRef><a:effectRef idx=\"0\"><a:schemeClr val=\"accent1\"/></a:effectRef><a:fontRef idx=\"minor\"><a:schemeClr val=\"lt1\"/></a:fontRef></p:style><p:txBody><a:bodyPr rtlCol=\"0\" anchor=\"ctr\"/><a:lstStyle/><a:p><a:pPr algn=\"ctr\"/><a:endParaRPr kumimoji=\"1\" lang=\"ja-JP\" altLang=\"en-US\"/></a:p></p:txBody></p:sp>";
+    }
 
 private:
-    size canvasSize;
-    size canvasOffset;
+    range rangeX;
+    range rangeY;
     enum objectType type;
+    std::vector<point> points;
+    std::vector<bezierPoint> curves;
 };
 
 baseObject::~baseObject()
@@ -68,79 +183,78 @@ baseObject::~baseObject()
 class drawPPTX
 {
 public:
-    drawPPTX()
+    drawPPTX(const size& _canvasSize, const size& _canvasOffset)
         : objectIdCounter(2)
-    {}
+        , rangeX(range(cMinRange, cMaxRange))
+        , rangeY(range(0.0, 1.0))
+        , canvasOffset(_canvasOffset)
+        , canvasSize(_canvasSize)
+    {};
     ~drawPPTX() {} ;
+    void drawHeader(std::ostream& os) const
+    {
+        os << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" << std::endl;
+        os << "<p:sld xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:p=\"http://schemas.openxmlformats.org/presentationml/2006/main\"><p:cSld><p:spTree>";
+        os << "<p:nvGrpSpPr><p:cNvPr id=\"1\" name=\"\"/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr><p:grpSpPr><a:xfrm><a:off x=\"0\" y=\"0\"/><a:ext cx=\"0\" cy=\"0\"/><a:chOff x=\"0\" y=\"0\"/><a:chExt cx=\"0\" cy=\"0\"/></a:xfrm></p:grpSpPr>";
+    }
+    void drawFooter(std::ostream& os) const
+    {
+        os << "</p:spTree><p:extLst><p:ext uri=\"{BB962C8B-B14F-4D97-AF65-F5344CB8AC3E}\"><p14:creationId xmlns:p14=\"http://schemas.microsoft.com/office/powerpoint/2010/main\" val=\"2550586031\"/></p:ext></p:extLst></p:cSld><p:clrMapOvr><a:masterClrMapping/></p:clrMapOvr></p:sld>";
+    }
+    void drawObjects(std::ostream& os) const
+    {
+        for (auto&& it : objects)
+        {
+            switch (it.getType())
+            {
+            case OBJECT_CURVES:
+                it.drawObject(os, objectIdCounter, canvasOffset, canvasSize);
+                break;
+            case OBJECT_STRAIGHTLINE:
+            default:
+                break;
+            }
+        }
+    }
+    void push_back(const baseObject& obj)
+    {
+        objects.push_back(obj);
+        if (obj.getType() == OBJECT_CURVES)
+        {
+            rangeX = obj.getRangeX();
+            rangeY = obj.getRangeY();
+        }
+    }
 
 private:
     std::vector<baseObject> objects;
+    size canvasSize, canvasOffset;
     unsigned int objectIdCounter;
+    range rangeX, rangeY;
 };
-
-double convertToCanvasY(double y, double maxRangeY, double minRangeY, double canvasHeight)
-{
-    double ratioY = canvasHeight / (maxRangeY - minRangeY);
-    double result = (y - minRangeY) * ratioY;
-    result = canvasHeight - result;
-    return result;
-}
-
-point convertToCanvas(const point& figurePoint, const double minRangeX, const double maxRangeX, const double minRangeY, const double maxRangeY, const int canvasWidth, const int canvasHeight)
-{
-    point result;
-    double ratioX = canvasWidth / (maxRangeX - minRangeX);
-    result.x = (figurePoint.x - minRangeX) * ratioX;
-    result.y = convertToCanvasY(figurePoint.y, maxRangeY, minRangeY, canvasHeight);
-    return result;
-}
 
 std::ostream& operator << (std::ostream& os, const drawPPTX& a)
 {
     // output header
-    os << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" << std::endl;
-    os << "<p:sld xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:p=\"http://schemas.openxmlformats.org/presentationml/2006/main\"><p:cSld><p:spTree>";
-    os << "<p:nvGrpSpPr><p:cNvPr id=\"1\" name=\"\"/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr><p:grpSpPr><a:xfrm><a:off x=\"0\" y=\"0\"/><a:ext cx=\"0\" cy=\"0\"/><a:chOff x=\"0\" y=\"0\"/><a:chExt cx=\"0\" cy=\"0\"/></a:xfrm></p:grpSpPr>" << std::endl;
-    os << "<p:sp><p:nvSpPr><p:cNvPr id=\"2\" name=\"qqqqqqqqq\"><a:extLst><a:ext uri=\"{FF2B5EF4-FFF2-40B4-BE49-F238E27FC236}\"><a16:creationId xmlns:a16=\"http://schemas.microsoft.com/office/drawing/2014/main\" id=\"{29FFCF3D-8F32-482F-B193-ACC1CF50B0FA}\"/></a:ext></a:extLst></p:cNvPr><p:cNvSpPr/><p:nvPr/></p:nvSpPr>";
+    a.drawHeader(os);
+    a.drawObjects(os);
+    a.drawFooter(os);
     return os;
 }
 
 int main(int argc, char**argv)
 {
-    const double cStep = 0.1;
-    const int cIteration = (int)((cMaxRange - cMinRange + DBL_EPSILON) / cStep);
+    drawPPTX normalDistribution(size(cCanvasWidth, cCanvasHeight), size(cCanvasOffsetX, cCanvasOffsetY));
 
-    bezierPoint dPoint;
-    std::vector<point> points;
-    std::vector<bezierPoint> curves;
-    double minY = DBL_MAX;
-    double maxY = -DBL_MAX;
-    for (size_t i = 0; i <= cIteration; i++)
-    {
-        point pt;
-        pt.x = cMinRange + i * cStep;
-        pt.y = function(pt.x);
-        double dStep = cStep / 3.;
-        double slope = (function(pt.x + dStep) - function(pt.x - dStep)) / (dStep * 2);
-        double dx = pt.x + dStep;
-        points.push_back(pt);
-        dPoint.pt[1].x = pt.x - dStep;
-        dPoint.pt[1].y = pt.y - slope * dStep;
-        dPoint.pt[2].x = pt.x;
-        dPoint.pt[2].y = pt.y;
-        minY = pt.y < minY ? pt.y : minY;
-        maxY = pt.y > maxY ? pt.y : maxY;
-        if (i != 0)
-        { // skip first iteration
-            curves.push_back(dPoint);
-        }
-        dPoint.pt[0].x = dx;
-        dPoint.pt[0].y = pt.y + slope * dStep;
-    }
-    int canvasHeight = (int)((maxY - minY) * cVerticalScale) * cm2pptx;
+    range rangeX(cMinRange, cMaxRange);
+    baseObject mainCurve(OBJECT_CURVES);
+    mainCurve.drawCurves(function, rangeX, cStepCounts);
+
+    normalDistribution.push_back(mainCurve);
 
     std::ofstream ofs("slide1.xml");
-#if 1
+    ofs << normalDistribution;
+#if 0
     ofs << "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>" << std::endl;
     ofs << "<p:sld xmlns:a=\"http://schemas.openxmlformats.org/drawingml/2006/main\" xmlns:r=\"http://schemas.openxmlformats.org/officeDocument/2006/relationships\" xmlns:p=\"http://schemas.openxmlformats.org/presentationml/2006/main\"><p:cSld><p:spTree><p:nvGrpSpPr><p:cNvPr id=\"1\" name=\"\"/><p:cNvGrpSpPr/><p:nvPr/></p:nvGrpSpPr><p:grpSpPr><a:xfrm><a:off x=\"0\" y=\"0\"/><a:ext cx=\"0\" cy=\"0\"/><a:chOff x=\"0\" y=\"0\"/><a:chExt cx=\"0\" cy=\"0\"/></a:xfrm></p:grpSpPr>";
     ofs << "<p:sp><p:nvSpPr><p:cNvPr id=\"2\" name=\"qqqqqqqqq\"><a:extLst><a:ext uri=\"{FF2B5EF4-FFF2-40B4-BE49-F238E27FC236}\"><a16:creationId xmlns:a16=\"http://schemas.microsoft.com/office/drawing/2014/main\" id=\"{29FFCF3D-8F32-482F-B193-ACC1CF50B0FA}\"/></a:ext></a:extLst></p:cNvPr><p:cNvSpPr/><p:nvPr/></p:nvSpPr>";
